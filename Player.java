c7eae24def7716877c1e5573c455739adce69b51
kmatsuyoshi <kmatsuyoshi@stuy.edu>
2018-05-29 10:58:05 -0400

    fixed proposal

---
+++ Player.java
@@ -0,0 +1,8 @@
+// CoderzWitAttitudes ( Fiona Cai, Kayli Matsuyoshi, Ben Platt )
+// APCS2 pd1
+// Final Project -- Island Survival Game
+// 2018-06-01f
+
+/*****************************************************
+ * class Player
+ *****************************************************/
