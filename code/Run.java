0823d748d92244342dd61f501975c8a66ae850f7
kmatsuyoshi <kmatsuyoshi@stuy.edu>
2018-06-08 00:06:48 -0400

    still runnning

--- code/Run.java
+++ code/Run.java
@@ -49,6 +49,7 @@ public class Run {
 	System.out.println("You find yourself stranded on a Tropical Island.");
 	System.out.println("You have " + player.getHealth() + " health.");
         while( player.getHealth() != 0 ) {
+	    
 	    System.out.println("What will you do now?");
 	    int code = mainMenu(scanner);
         
@@ -97,6 +98,202 @@ public class Run {
 	    }
 	    else if ( code == 5 ) {
 		// EAT
+		while(true) {
+		    System.out.println("What do you want to eat?");
+		    System.out.println("1. A Plant Food\n2. An Animal Food\n3. Nevermind...");
+		    input = scanner.nextLine();
+		    if ( input == "1" ) {
+			if ( player.getPlantFood().equals("Sorry you have no plants to eat") ) {
+			    System.out.println( player.getPlantFood() );
+			}
+			else {
+			    while(true) {
+				System.out.println("Which plant will you eat?");
+				String plants = player.getPlantFood();
+				int start = 3;
+				int end = 0;
+				ItemNode currentItem = player.getItems()._start;
+				String inputBreed = "";
+				System.out.println( plants );
+				input = scanner.nextLine();
+				if ( input == "1" ) {
+				    end = plants.indexOf( "\\", start );
+				    inputBreed = plants.substring( start, end );
+				    while( currentItem.getContents() != inputBreed ){
+					currentItem = currentItem.getNext();
+				    }
+				    player.eatPlant( currentItem.getItem().peek() );
+				    break;
+				}
+				else if ( input == "2" ) {
+				    if ( plants.contains( input ) ) {
+					start = plants.indexOf( input, 0 ) + 3;
+					end = plants.indexOf( "\\", start );
+					inputBreed = plants.substring( start, end );
+					while( currentItem.getContents() != inputBreed ){
+					    currentItem = currentItem.getNext();
+					}
+					player.eatPlant( currentItem.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+				else if ( input == "3" ) {
+				    if ( plants.contains( input ) ) {
+					start = plants.indexOf( input, 0 ) + 3;
+					end = plants.indexOf( "\\", start );
+					inputBreed = plants.substring( start, end );
+					while( currentItem.getContents() != inputBreed ){
+					    currentItem = currentItem.getNext();
+					}
+					player.eatPlant( currentItem.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+				else if ( input == "4" ) {
+				    if ( plants.contains( input ) ) {
+					start = plants.indexOf( input, 0 ) + 3;
+					end = plants.indexOf( "\\", start );
+					inputBreed = plants.substring( start, end );
+					while( currentItem.getContents() != inputBreed ){
+					    currentItem = currentItem.getNext();
+					}
+					player.eatPlant( currentItem.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+				else if ( input == "5" ) {
+				    if ( plants.contains( input ) ) {
+					start = plants.indexOf( input, 0 ) + 3;
+					end = plants.indexOf( "\\", start );
+					inputBreed = plants.substring( start, end );
+					while( currentItem.getContents() != inputBreed ){
+					    currentItem = currentItem.getNext();
+					}
+					player.eatPlant( currentItem.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+			    }
+			}
+		    }
+		    else if ( input == "2" ) {
+			if ( player.getAnimalFood().equals("Sorry you have no animals to eat") ) {
+			    System.out.println( player.getAnimalFood() );
+			}
+			else {
+			    while(true) {
+				System.out.println("Which animal will you eat?");
+				String animals = player.getAnimalFood();
+				int starta = 3;
+				int enda = 0;
+				ItemNode currentItema = player.getItems()._start;
+				String inputBreeda = "";
+				System.out.println( animals );
+				input = scanner.nextLine();
+				if ( input == "1" ) {
+				    enda = animals.indexOf( "\\", starta );
+				    inputBreeda = animals.substring( starta, enda );
+				    while( currentItema.getContents() != inputBreed ){
+					currentItema = currentItema.getNext();
+				    }
+				    player.eatAnimal( currentItema.getItem().peek() );
+				    break;
+				}
+				else if ( input == "2" ) {
+				    if ( animals.contains( input ) ) {
+					starta = animals.indexOf( input, 0 ) + 3;
+					enda = animals.indexOf( "\\", starta );
+				        inputBreeda = animals.substring( starta, enda );
+					while( currentItema.getContents() != inputBreed ){
+					    currentItema = currentItema.getNext();
+					}
+					player.eatAnimal( currentItema.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+				else if ( input == "3" ) {
+				    if ( animals.contains( input ) ) {
+					starta = animals.indexOf( input, 0 ) + 3;
+					enda = animals.indexOf( "\\", starta );
+				        inputBreeda = animals.substring( starta, enda );
+					while( currentItema.getContents() != inputBreed ){
+					    currentItema = currentItema.getNext();
+					}
+					player.eatAnimal( currentItema.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+				else if ( input == "4" ) {
+				    if ( animals.contains( input ) ) {
+					starta = animals.indexOf( input, 0 ) + 3;
+					enda = animals.indexOf( "\\", starta );
+					inputBreeda = animals.substring( starta, enda );
+					while( currentItema.getContents() != inputBreed ){
+					    currentItema = currentItema.getNext();
+					}
+					player.eatAnimal( currentItema.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+				else if ( input == "5" ) {
+				    if ( animals.contains( input ) ) {
+					starta = animals.indexOf( input, 0 ) + 3;
+					enda = animals.indexOf( "\\", starta );
+				        inputBreeda = animals.substring( starta, enda );
+					while( currentItema.getContents() != inputBreed ){
+					    currentItema = currentItema.getNext();
+					}
+					player.eatAnimal( currentItema.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+				else if ( input == "6" ) {
+				    if ( animals.contains( input ) ) {
+					starta = animals.indexOf( input, 0 ) + 3;
+					enda = animals.indexOf( "\\", starta );
+					inputBreeda = animals.substring( starta, enda );
+					while( currentItema.getContents() != inputBreed ){
+					    currentItema = currentItema.getNext();
+					}
+					player.eatAnimal( currentItema.getItem().peek() );
+					break;
+				    }
+				    else {
+					System.out.println( "Invalid Input! Try Again!");
+				    }					 
+				}
+			    }
+			}
+		    }
+		    else if ( input == "3" ) {
+			break;
+		    }
+		}
+		
 	    }
 	    //break;
 	
