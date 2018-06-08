6742cf48d02008cdafdff1f6ac11ed6a1dd2828a
kmatsuyoshi <kmatsuyoshi@stuy.edu>
2018-06-08 01:31:13 -0400

    yes

--- code/Run.java
+++ code/Run.java
@@ -13,9 +13,9 @@ public class Run {
 	int code = -1;		
 	String choice = "";
 	while(true) {
-	    System.out.println("1. Go to Shelter\n2. Hunt\n3. Gather Plants\n4. Fish\n5. Eat");
+	    System.out.println("1. Go to Shelter\n2. Hunt\n3. Gather Plants\n4. Fish\n5. Eat\n6. List Items");
 	    choice = scanner.nextLine();		
-	    if (choice.equals("1") || choice.equals("2") || choice.equals("3") || choice.equals("4") || choice.equals("5") ) {
+	    if (choice.equals("1") || choice.equals("2") || choice.equals("3") || choice.equals("4") || choice.equals("5") || choice.equals("6") ) {
 		code = Integer.parseInt(choice);
 		break;
 	    } else {
@@ -60,11 +60,11 @@ public class Run {
 			System.out.println("What will you do?");
 			System.out.println("1. Sleep\n2. Exit shelter");
 			input = scanner.nextLine();
-			if ( input == "1" ) {
+			if ( input.equals("1") ) {
 			    player.sleep();
 			    break;
 			}
-			else if ( input == "2" ) {
+			else if ( input.equals("2") ) {
 			    break;
 			}
 		    }
@@ -75,17 +75,17 @@ public class Run {
 			System.out.println("What will you do?");
 			System.out.println("1. Build a Shelter\n2. Nevermind...");
 			input = scanner.nextLine();
-			if ( input == "1" ) {
+			if ( input.equals("1") ) {
 			    player.buildShelter(); // this dont work...
+			    System.out.println( "please" );
 			    break;
 			}
-			else if ( input == "2" ) {
+			else if ( input.equals("2") ) {
 			    break;
 			}
 		    }
 		}
-		    
-	    }
+	    }	    
 	    else if ( code == 2 ) {
 		// HUNT
 	        player.attack();
@@ -102,7 +102,7 @@ public class Run {
 		    System.out.println("What do you want to eat?");
 		    System.out.println("1. A Plant Food\n2. An Animal Food\n3. Nevermind...");
 		    input = scanner.nextLine();
-		    if ( input == "1" ) {
+		    if ( input.equals("1") ) {
 			if ( player.getPlantFood().equals("Sorry you have no plants to eat") ) {
 			    System.out.println( player.getPlantFood() );
 			}
@@ -116,7 +116,7 @@ public class Run {
 				String inputBreed = "";
 				System.out.println( plants );
 				input = scanner.nextLine();
-				if ( input == "1" ) {
+				if ( input.equals("1") ) {
 				    end = plants.indexOf( "\\", start );
 				    inputBreed = plants.substring( start, end );
 				    while( currentItem.getContents() != inputBreed ){
@@ -125,7 +125,7 @@ public class Run {
 				    player.eatPlant( currentItem.getItem().peek() );
 				    break;
 				}
-				else if ( input == "2" ) {
+				else if ( input.equals("2") ) {
 				    if ( plants.contains( input ) ) {
 					start = plants.indexOf( input, 0 ) + 3;
 					end = plants.indexOf( "\\", start );
@@ -140,7 +140,7 @@ public class Run {
 					System.out.println( "Invalid Input! Try Again!");
 				    }					 
 				}
-				else if ( input == "3" ) {
+				else if ( input.equals("3") ) {
 				    if ( plants.contains( input ) ) {
 					start = plants.indexOf( input, 0 ) + 3;
 					end = plants.indexOf( "\\", start );
@@ -155,7 +155,7 @@ public class Run {
 					System.out.println( "Invalid Input! Try Again!");
 				    }					 
 				}
-				else if ( input == "4" ) {
+				else if ( input.equals("4") ) {
 				    if ( plants.contains( input ) ) {
 					start = plants.indexOf( input, 0 ) + 3;
 					end = plants.indexOf( "\\", start );
@@ -170,7 +170,7 @@ public class Run {
 					System.out.println( "Invalid Input! Try Again!");
 				    }					 
 				}
-				else if ( input == "5" ) {
+				else if ( input.equals("5") ) {
 				    if ( plants.contains( input ) ) {
 					start = plants.indexOf( input, 0 ) + 3;
 					end = plants.indexOf( "\\", start );
@@ -188,7 +188,7 @@ public class Run {
 			    }
 			}
 		    }
-		    else if ( input == "2" ) {
+		    else if ( input.equals("2") ) {
 			if ( player.getAnimalFood().equals("Sorry you have no animals to eat") ) {
 			    System.out.println( player.getAnimalFood() );
 			}
@@ -202,7 +202,7 @@ public class Run {
 				String inputBreeda = "";
 				System.out.println( animals );
 				input = scanner.nextLine();
-				if ( input == "1" ) {
+				if ( input.equals("1") ) {
 				    enda = animals.indexOf( "\\", starta );
 				    inputBreeda = animals.substring( starta, enda );
 				    while( currentItema.getContents() != inputBreeda ){
@@ -211,7 +211,7 @@ public class Run {
 				    player.eatAnimal( currentItema.getItem().peek() );
 				    break;
 				}
-				else if ( input == "2" ) {
+				else if ( input.equals("2") ) {
 				    if ( animals.contains( input ) ) {
 					starta = animals.indexOf( input, 0 ) + 3;
 					enda = animals.indexOf( "\\", starta );
@@ -226,7 +226,7 @@ public class Run {
 					System.out.println( "Invalid Input! Try Again!");
 				    }					 
 				}
-				else if ( input == "3" ) {
+				else if ( input.equals("3") ) {
 				    if ( animals.contains( input ) ) {
 					starta = animals.indexOf( input, 0 ) + 3;
 					enda = animals.indexOf( "\\", starta );
@@ -241,7 +241,7 @@ public class Run {
 					System.out.println( "Invalid Input! Try Again!");
 				    }					 
 				}
-				else if ( input == "4" ) {
+				else if ( input.equals("4") ) {
 				    if ( animals.contains( input ) ) {
 					starta = animals.indexOf( input, 0 ) + 3;
 					enda = animals.indexOf( "\\", starta );
@@ -256,7 +256,7 @@ public class Run {
 					System.out.println( "Invalid Input! Try Again!");
 				    }					 
 				}
-				else if ( input == "5" ) {
+				else if ( input.equals("5") ) {
 				    if ( animals.contains( input ) ) {
 					starta = animals.indexOf( input, 0 ) + 3;
 					enda = animals.indexOf( "\\", starta );
@@ -271,7 +271,7 @@ public class Run {
 					System.out.println( "Invalid Input! Try Again!");
 				    }					 
 				}
-				else if ( input == "6" ) {
+				else if ( input.equals("6") ) {
 				    if ( animals.contains( input ) ) {
 					starta = animals.indexOf( input, 0 ) + 3;
 					enda = animals.indexOf( "\\", starta );
@@ -289,13 +289,15 @@ public class Run {
 			    }
 			}
 		    }
-		    else if ( input == "3" ) {
+		    else if ( input.equals("3") ) {
 			break;
 		    }
 		}
 		
 	    }
-	    //break;
+	    else if ( code == 6 ) {
+		player.getItems().display();
+	    }
 	
 	}
 	
